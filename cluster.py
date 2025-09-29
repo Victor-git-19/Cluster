4161a54f874e13b1c8273b388718c580f114f979
Виктор Смирнов <vitorsmirnow@yandex.ru>
2025-09-29 15:51:48 +0500

    Cluster

--- cluster.py
+++ cluster.py
@@ -2,7 +2,6 @@ from collections import deque
 import heapq
 
 
-# Job`s class
 class Job:
     """Job Class"""
     def __init__(self, name, duration, nodes_required, priority=0):
@@ -16,3 +15,28 @@ class Job:
         return (
             f"{self.name}(dur={self.duration},\n"
             f"nodes={self.nodes_required}, pri={self.priority})")
+
+
+class Cluster:
+    """Cluster Class"""
+    def __init__(self, total_nodes):
+        self.total_nodes = total_nodes
+        self.available_nodes = total_nodes
+        self.running = []
+
+    def can_run(self, job):
+        return self.available_nodes >= job.nodes_required
+
+    def start_job(self, job):
+        self.available_nodes -= job.nodes_required
+        self.running.append(job)
+
+    def tick(self):
+        finished = []
+        for job in list(self.running):
+            job.remaining -= 1
+            if job.remaining <= 0:
+                finished.append(job)
+                self.running.remove(job)
+                self.available_nodes += job.nodes_required
+        return finished
